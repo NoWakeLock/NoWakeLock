d66a18b744edae0112a5ae33a1b33d191401db96
JS <ljy087621@gmail.com>
2025-05-03 23:10:16 +0800

    refactor: Comment out @SuppressLint annotations in SPTools for improved code clarity and maintainability.

--- app/src/main/java/com/js/nowakelock/base/SPTools.kt
+++ app/src/main/java/com/js/nowakelock/base/SPTools.kt
@@ -12,7 +12,7 @@ class SPTools {
         private const val default_str = ""
         private const val default_bool = true
 
-        @SuppressLint("WorldReadableFiles")
+        // @SuppressLint("WorldReadableFiles")
         private var prefs = try {
             // MODE_WORLD_READABLE, New XSharedPreferences
             BasicApp.context.getSharedPreferences(SP_NAME, Context.MODE_WORLD_READABLE)
@@ -25,7 +25,7 @@ class SPTools {
             return prefs?.getString(key, defaultValue) ?: defaultValue
         }
 
-        @SuppressLint("CommitPrefEdits")
+        // @SuppressLint("CommitPrefEdits")
         fun setString(key: String, value: String) {
             with(prefs?.edit() ?: return) {
                 putString(key, value)
@@ -38,7 +38,7 @@ class SPTools {
             return prefs?.getBoolean(key, defaultValue) ?: defaultValue
         }
 
-        @SuppressLint("CommitPrefEdits")
+        // @SuppressLint("CommitPrefEdits")
         fun setBoolean(key: String, value: Boolean) {
             with(prefs?.edit() ?: return) {
                 putBoolean(key, value)
@@ -51,7 +51,7 @@ class SPTools {
             return prefs?.getInt(key, defaultValue) ?: defaultValue
         }
 
-        @SuppressLint("CommitPrefEdits")
+        // @SuppressLint("CommitPrefEdits")
         fun setInt(key: String, value: Int) {
             with(prefs?.edit() ?: return) {
                 putInt(key, value)
@@ -63,7 +63,7 @@ class SPTools {
             return prefs?.getLong(key, defaultValue) ?: defaultValue
         }
 
-        @SuppressLint("CommitPrefEdits")
+        // @SuppressLint("CommitPrefEdits")
         fun setLong(key: String, value: Long) {
             with(prefs?.edit() ?: return) {
                 putLong(key, value)
